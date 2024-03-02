7edf46655e3e5b1aa58cab30c808b48527c9b633
Abdo <abdo@abdnh.net>
2024-03-02 05:13:33 +0300

    Support keyboard navigation

--- popup.js
+++ popup.js
@@ -1,5 +1,33 @@
 function setupSearchbar() {
     const resultsContainer = document.getElementById("anki-search-results");
+    document.body.addEventListener("keydown", (event) => {
+        const results = resultsContainer.children;
+        if (!results.length) return;
+        const selected = Array.from(results).findIndex((el) =>
+            el.classList.contains("selected")
+        );
+        if (selected >= 0 && event.key === "Enter") {
+            results[selected].click();
+            return;
+        }
+        let newSelected;
+        if (event.key === "ArrowDown") {
+            newSelected = (selected + 1) % results.length;
+        } else if (event.key === "ArrowUp") {
+            if (selected <= 0) {
+                newSelected = results.length - 1;
+            } else {
+                newSelected = selected - 1;
+            }
+        } else {
+            return;
+        }
+        if (selected >= 0) {
+            results[selected].classList.remove("selected");
+        }
+        results[newSelected].classList.add("selected");
+        results[newSelected].scrollIntoView({ block: "center" });
+    });
     const input = document.querySelector("input");
     chrome.runtime.sendMessage("get-links", (links) => {
         input.addEventListener("input", () => {
