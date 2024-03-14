1290fb976d4cf7d001cd3afd42ec92a5a4f6dcdf
Abdo <abdo@abdnh.net>
2024-03-14 20:58:33 +0300

    Extract anchors

--- scripts/extract_links.py
+++ scripts/extract_links.py
@@ -6,6 +6,7 @@ Usage:
 2. Run: `./update-links.sh``
 """
 
+import sys
 from urllib.request import urlopen
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
@@ -19,5 +20,12 @@ SITES = [
 for site in SITES:
     with urlopen(site) as response:
         soup = BeautifulSoup(response.read(), "html.parser")
-        for link in soup.select(".chapter-item a[href]"):
-            print(urljoin(site, link["href"]))
+        for chapter_anchor in soup.select(".chapter-item a[href]"):
+            chapter_url = urljoin(site, chapter_anchor["href"])
+            print(chapter_url)
+            print(f"Extracting links from {chapter_url}", file=sys.stderr)
+            with urlopen(chapter_url) as chapter_response:
+                chapter_soup = BeautifulSoup(chapter_response.read(), "html.parser")
+                for header_anchor in chapter_soup.select(".header")[1:]:  # skip h1
+                    header_url = urljoin(chapter_url, header_anchor["href"])
+                    print(header_url)
